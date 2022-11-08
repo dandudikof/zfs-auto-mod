302702985e6b03ed15191708e574fc53ad95caef
dan dudikof <dan@prodesk.lan>
2022-11-08 04:40:09 -0500

    Added fnc_lock-check.sh
    added peer script peer config locking
    
    each script gets its own subfolder in run_dir where it keeps config.lock
    (can run a script while any other script is also running on same config file)

---
+++ fnc_lock-check.sh
@@ -0,0 +1,82 @@
+#
+# checks for existence of lockfile for execution of current script with current *.cfg file
+# exits with 1 if lockfile exist and pid that created it is still running
+# else removes stale lock files, creates new lock files, returns 0 and continues
+#
+
+
+
+pid=$$
+script="$(basename $0)"
+config_file="$(basename $1)"
+config_path="$(readlink -f $1)"
+lock_file="$lock_dir/$script/$config_file.lock"
+pid_file="$lock_dir/$script/$config_file.pid"
+
+
+
+do_lock_check() {
+printf "\n---------------------------------- do_lock_check --------------------------------\n" 1>&4
+		# lock files creation and lock checks
+
+[ ! -d "$lock_dir" ] && mkdir "$lock_dir"
+[ ! -d "$lock_dir/$script" ] && mkdir "$lock_dir/$script"
+
+if [ -f $lock_file ] ;then
+	
+	local config_pid="$(cat $pid_file)"
+	
+	ps -p "$config_pid" > /dev/null 2>&1
+	if [ "$?" = 0 ]  ;then
+	
+		echo "[ERROR] lock files exist for $script $config_file and script is running , exit 1" 1>&3
+		exit 1
+		
+	else
+	
+		echo "[INFO2] lock files exist for $script $config_file but are stale , removing" 1>&4
+		rm "$lock_file"
+		rm "$pid_file"
+	
+	fi
+
+fi
+
+if [ -d "$lock_dir/$script" ] ;then
+
+	echo "[info2] creating lock_file for $script $config_file" 1>&4
+	echo "[info2] creating pid_file for $script $config_file" 1>&4
+	echo "$config_path" > "$lock_file"
+	echo "$pid" > "$pid_file"
+
+else
+
+	echo "[ERROR] unable to create lock files in $lock_dir/$script , locking functionality diabled" 1>&3
+
+fi
+
+return 0
+
+}
+
+
+
+do_lock_clear() {
+printf "\n---------------------------------- do_lock_clear --------------------------------\n" 1>&4
+
+
+
+if [ -f $lock_file ] ;then
+	
+		echo "[INFO2] removing lock_file for $script $config_file" 1>&4
+		echo "[INFO2] removing pid_file for $script $config_file" 1>&4
+		rm "$lock_file"
+		rm "$pid_file"
+fi
+
+return 0
+
+}
+
+
+
