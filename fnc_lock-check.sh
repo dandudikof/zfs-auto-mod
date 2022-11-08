ef0ebd72877d143d66d6cfe0ffe6d471b766134c
dan dudikof <dan@prodesk.lan>
2022-11-08 04:40:09 -0500

    Added fnc_lock-check.sh
    Only peer config locking
    (Tested outside of scripts)

---
+++ fnc_lock-check.sh
@@ -0,0 +1,76 @@
+#
+# checks for existence of lockfile for execution of current *.cfg file
+# exits with 1 if lockfile exist and pid that created it is still running
+# else removes stale lock files, creates new lock files, returns 0 and continues
+#
+
+
+
+lock_dir="/run/lock/zfs-auto-mod"
+
+pid=$$
+config_file="$(basename $1)"
+config_path="$(readlink -f $1)"
+lock_file="$lock_dir/$config_file.lock"
+pid_file="$lock_dir/$config_file.pid"
+
+
+[ ! -d "$lock_dir" ] && mkdir -p "$lock_dir"
+
+
+
+do_lock_check() {
+printf "\n---------------------------------- do_lock_check --------------------------------\n" 1>&4
+			# tested outside of scripts
+
+
+if [ -f $lock_file ] ;then
+	
+	local config_pid="$(cat $pid_file)"
+	
+	ps -p "$config_pid" > /dev/null 2>&1
+	if [ "$?" = 0 ]  ;then
+	
+		echo "[ERROR] lock files exist for $config_file and script is running , exit 1" 1>&3
+		exit 1
+		
+	else
+	
+		echo "[INFO2] lock files exist for $config_file but are stale , removing" 1>&4
+		rm "$lock_file"
+		rm "$pid_file"
+	
+	fi
+
+fi
+
+echo "[INFO2] creating lock_file for $config" 1>&4
+echo "[INFO2] creating pid_file for $config" 1>&4
+echo "$config_path" > "$lock_file"
+echo "$pid" > "$pid_file"
+
+return 0
+
+}
+
+
+
+do_lock_clear() {
+printf "\n---------------------------------- do_lock_clear --------------------------------\n" 1>&4
+
+
+
+if [ -f $lock_file ] ;then
+	
+		echo "[INFO2] removing lock_file for $config" 1>&4
+		echo "[INFO2] removing pid_file for $config" 1>&4
+		rm "$lock_file"
+		rm "$pid_file"
+fi
+
+return 0
+
+}
+
+
+
